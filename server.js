2eae15ef3e41e474feed6a63c135c1045be23397
Mehdi <mehdihannachi02@gmail.com>
2021-02-11 11:18:38 +0100

    make html template for testing

--- server.js
+++ server.js
@@ -8,9 +8,19 @@ const app = express();
 
 // Create the endpoints
 
-app.get("/", (req, res) => {
-  res.send("Welcome to Express Introduction");
-});
+// app.get("/", (req, res) => {
+//   res.send("Welcome to Express Introduction");
+// });
+
+//Show one file
+
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/public/contact.html");
+// });
+
+//Show all directory files
+
+app.use(express.static(__dirname + "/public"));
 
 // Run Server
 
